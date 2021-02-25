68d580a9fa188490ccf6dfdc1793356968227469
cengjingbaibai <1179590550@qq.com>
2021-02-26 00:19:52 +0800

    1

--- src/frame/TankFrame.java
+++ src/frame/TankFrame.java
@@ -1,4 +1,33 @@
 package frame;
 
-public class TrankFrame {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+	int x = 200, y = 200;
+	public TankFrame() throws HeadlessException {
+		setSize(800,600);
+		setResizable(false);
+		setTitle("tank war");
+		setVisible(true);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		System.out.println("paint");
+		g.fillRect(x,y,50,50);
+		x += 10;
+		y += 10;
+	}
+
+	public static void main(String[] args) {
+		TankFrame tankFrame = new TankFrame();
+	}
 }
